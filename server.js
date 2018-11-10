27241dd10fc859e8ab1c869cca89103331ad9432
bigLifeGit <vivekthejoker777@gmail.com>
2018-11-10 10:13:50 -0500

    io socket connection

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const publicPath = path.join(__dirname , './public/index.html');
+const http = require('http');
+var app = express();
+const publicPath = path.join(__dirname, './public/index.html');
 const port = process.env.PORT || 3000;
+var socketIO = require('socket.io');
+
+
 
 app.use(express.static(publicPath));
- 
+
 
 console.log(publicPath);
 
-app.get('/',function(req,res){
-       
+app.get('/',function (req, res)  {
+
     res.sendFile(publicPath);
 
 });
 
-app.listen(port , ()=>{ console.log(`       express running on  ${port}`);
+var server = http.createServer(app).listen(3000);
+
+var io = socketIO(server).listen(server);
+
+io.on('connection' , (socket)=>{
+
+     console.log('New user connected');
+    
+     socket.on('disconnect' , ()=>{console.log('User disconnected');
+     });
 });
+
+
