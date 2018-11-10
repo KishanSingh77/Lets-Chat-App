adaf1307807c813d896bbbd354f6b8742f66470a
bigLifeGit <vivekthejoker777@gmail.com>
2018-11-10 00:25:54 -0500

    changes to node chat folder

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const publicPath = path.join(__dirname , '../public');
+const publicPath = path.join(__dirname , './public/index.html');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+ 
 
 console.log(publicPath);
+
+app.get('/',function(req,res){
+       
+    res.sendFile(publicPath);
+
+});
 
 app.listen(port , ()=>{ console.log(`       express running on  ${port}`);
 });
